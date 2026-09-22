f4b01a32f3efbf209f5d40cabb680281ddff9e7d
Nazario Escoto <nazario@uamv.edu.ni>
2026-09-22 16:37:17 -0600

    feat: Utils terminados

---
+++ src/main/java/ni/edu/uam/evaluacionpracticaic/util/AlertHelper.java
@@ -0,0 +1,37 @@
+package ni.edu.uam.empleados.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+public class AlertHelper {
+
+    public static void informacion(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public static void error(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public static boolean confirmar(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+
+        Optional<ButtonType> resultado = alert.showAndWait();
+
+        return resultado.isPresent()
+                && resultado.get() == ButtonType.OK;
+    }
+}
